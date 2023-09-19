15151828a57e70214026ee2b72b51118620af4a3
Hitesh N P <97424880+HiteshNP@users.noreply.github.com>
2023-09-19 16:21:56 +0530

    Update p9a.py

--- p9a.py
+++ p9a.py
@@ -1,22 +1,22 @@
-"""Write a python program to download the all XKCD comics"""
-
 import requests as req
-import os, bs4
+import os
+import bs4
 
 url = 'https://xkcd.com/'
-os.makedirs('xkcd', exist_ok=True)
+if not os.path.isdir('xkcd'):
+    os.makedirs('xkcd')
 
-for i in range(1, 8):
-    print('Downloading image from %s...' % url)
-    res = req.get(url)
+for i in range(100, 110):
+    temp = url + str(i)
+    print('Downloading image from %s...' % temp)
+    res = req.get(temp)
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     comicElem = soup.select('#comic img')
-    comicUrl = 'http:' + comicElem[0].get('src')
+    comicUrl = 'https:' + comicElem[0].get('src')
+    res = req.get(comicUrl)
+    imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
 
-res = req.get(comicUrl)
-
-imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-for chunk in res.iter_content(1):
-    imageFile.write(chunk)
-imageFile.close()
 print('Successfully downloaded')
