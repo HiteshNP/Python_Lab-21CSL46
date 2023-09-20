d9bb2339362cda25cc223c79e25651197e067b52
Hitesh N P <97424880+HiteshNP@users.noreply.github.com>
2023-09-20 15:27:33 +0530

    Update p7a.py

--- p7a.py
+++ p7a.py
@@ -5,59 +5,65 @@ import math
 
 
 class Shape:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
         self.area = 0
+        self.name = ""
 
-    def calculate_area(self):
-        pass
-
-    def show_area(self):
+    def showArea(self):
         print("The area of the", self.name, "is", self.area, "units")
 
 
 class Circle(Shape):
     def __init__(self, radius):
-        super().__init__("Circle")
+
+        super().__init__()
+        self.area = 0
+        self.name = "Circle"
         self.radius = radius
 
-    def calculate_area(self):
+    def calcArea(self):
         self.area = math.pi * self.radius * self.radius
 
 
 class Rectangle(Shape):
     def __init__(self, length, breadth):
-        super().__init__("Rectangle")
+
+        super().__init__()
+        self.area = 0
+        self.name = "Rectangle"
         self.length = length
         self.breadth = breadth
 
-    def calculate_area(self):
+    def calcArea(self):
         self.area = self.length * self.breadth
 
 
 class Triangle(Shape):
     def __init__(self, base, height):
-        super().__init__("Triangle")
+
+        super().__init__()
+        self.area = 0
+        self.name = "Triangle"
         self.base = base
         self.height = height
 
-    def calculate_area(self):
-        self.area = 0.5 * self.base * self.height
+    def calcArea(self):
+        self.area = self.base * self.height / 2
 
 
 r = int(input("Enter the radius of the circle: "))
 c1 = Circle(r)
-c1.calculate_area()
-c1.show_area()
+c1.calcArea()
+c1.showArea()
 
 l = int(input("\nEnter the length of the rectangle: "))
 b = int(input("Enter the breadth of the rectangle: "))
 r1 = Rectangle(l, b)
-r1.calculate_area()
-r1.show_area()
+r1.calcArea()
+r1.showArea()
 
-base = int(input("\nEnter the base of the triangle: "))
+b2 = int(input("\nEnter the base of the triangle: "))
 h = int(input("Enter the height of the triangle: "))
-t1 = Triangle(base, h)
-t1.calculate_area()
-t1.show_area()
+t1 = Triangle(b2, h)
+t1.calcArea()
+t1.showArea()
